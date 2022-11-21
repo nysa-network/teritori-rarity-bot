914364917e1f255ef8219041015c295ddf1346a5
Albert Le Batteux <contact@albttx.tech>
2022-11-21 17:49:13 +0000

    chore: remove useless file

--- constants.ts
+++
@@ -1,5 +0,0 @@
-export const contracts = {
-  tns: "tori1wkwy0xh89ksdgj9hr347dyd2dw7zesmtrue6kfzyml4vdtz6e5wscs7038",
-  riot: "tori1mf6ptkssddfmxvhdx0ech0k03ktp6kf9yk59renau2gvht3nq2gqg87tkw",
-  riot_minter: "tori1j08452mqwadp8xu25kn9rleyl2gufgfjnv0sn8dvynynakkjukcq3vtuv2"
-}
